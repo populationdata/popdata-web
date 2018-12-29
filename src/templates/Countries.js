d08bb7262ee42683c766c6d0a579b31728bf065f
Julien Blin <julien.blin@gmail.com>
2018-12-29 18:06:22 -0500

    Better maps.

--- src/templates/Countries.js
+++ src/templates/Countries.js
@@ -2,7 +2,19 @@ import React from 'react'
 import { css } from 'glamor'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import ColBlockItems from '../components/ColBlockItems'
 import { HTMLContent } from '../components/Content'
+
+const allLabels = {
+  fr: {
+    maps: 'Cartes',
+  },
+  en: {
+    maps: 'Maps',
+  },
+}
+
+const labels = allLabels[process.env.GATSBY_LANGUAGE]
 
 const contentCss = css({
   '& img': {
@@ -13,7 +25,21 @@ const contentCss = css({
 
 const CountryPage = ({ data }) => {
   return (
-    <Layout>
+    <Layout
+      col1={
+        <ColBlockItems
+          title={labels.maps}
+          items={data.maps.edges
+            .sort((a, b) =>
+              a.node.fields.title.localeCompare(b.node.fields.title)
+            )
+            .map(x => ({
+              link: x.node.fields.slug,
+              title: x.node.fields.title,
+            }))}
+        />
+      }
+    >
       <h1>{data.country.fields.name}</h1>
       <section>
         <HTMLContent
@@ -34,6 +60,17 @@ export const countryQuery = graphql`
       fields {
         name
         description
+      }
+    }
+    maps: allMapsYaml(filter: { countryNodeIds: { in: [$id] } }) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+            title
+          }
+        }
       }
     }
   }
--- src/templates/Maps.js
+++ src/templates/Maps.js
@@ -1,11 +1,82 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
+import { css } from 'glamor'
 import Layout from '../components/Layout'
+
+const allLabels = {
+  fr: {
+    continents: 'Continents :',
+    countries: 'Pays :',
+    seeAlso: 'Voir aussi',
+    source: 'Source :',
+  },
+  en: {
+    continents: 'Continents:',
+    countries: 'Countries:',
+    seeAlso: 'See also',
+    source: 'Source:',
+  },
+}
+
+const labels = allLabels[process.env.GATSBY_LANGUAGE]
+
+const mapImgCss = css({
+  marginTop: 20,
+  textAlign: 'center',
+})
+
+const sourceCss = css({
+  fontStyle: 'italic',
+})
+
+const seeAlsoCss = css({
+  marginTop: 20,
+})
 
 const MapPage = ({ data }) => {
   return (
     <Layout>
       <h1>{data.map.fields.title}</h1>
+      {data.map.type === 'image' && (
+        <div {...mapImgCss}>
+          <img
+            className="img-fluid"
+            src={data.map.image}
+            alt={data.map.fields.title}
+          />
+        </div>
+      )}
+      {data.map.source && (
+        <div {...sourceCss}>
+          {labels.source}
+          {data.map.source}
+        </div>
+      )}
+      <div {...seeAlsoCss}>
+        <h4>{labels.seeAlso}</h4>
+        {data.map.continents.length > 0 && (
+          <div>
+            {labels.continents}{' '}
+            {data.map.continents.map((x, index) => (
+              <>
+                <Link to={x.fields.slug}>{x.fields.name}</Link>
+                {index !== data.map.continents.length - 1 && ', '}
+              </>
+            ))}
+          </div>
+        )}
+        {data.map.countries.length > 0 && (
+          <div>
+            {labels.countries}{' '}
+            {data.map.countries.map((x, index) => (
+              <>
+                <Link to={x.fields.slug}>{x.fields.name}</Link>
+                {index !== data.map.countries.length - 1 && ', '}
+              </>
+            ))}
+          </div>
+        )}
+      </div>
     </Layout>
   )
 }
@@ -17,6 +88,21 @@ export const mapQuery = graphql`
     map: mapsYaml(id: { eq: $id }) {
       fields {
         title
+      }
+      image
+      source
+      type
+      continents {
+        fields {
+          name
+          slug
+        }
+      }
+      countries {
+        fields {
+          name
+          slug
+        }
       }
     }
   }
